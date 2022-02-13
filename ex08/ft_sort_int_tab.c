c4dbddd6a4078936f22457f07815f21b71d0b31d
Mehmet Gokyar <mgokyar@k1m14s06.42kocaeli.com.tr>
2022-02-13 17:54:34 +0300

    c_01 GİT HUB

---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgokyar <mgokyar@student.42kocaeli.com.tr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/13 17:13:08 by mgokyar           #+#    #+#             */
+/*   Updated: 2022/02/13 17:13:16 by mgokyar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	swap;
+
+	while (size >= 0)
+	{
+		i = 0;
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				swap = tab [i];
+				tab[i] = tab [i + 1];
+				tab [i + 1] = swap;
+			}
+			i++;
+		}
+		size--;
+	}
+}
